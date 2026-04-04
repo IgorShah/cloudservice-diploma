18dfa410b8b1a7f951dd3123aa5b988b4bdd6a2e
Igor Shahin <igor.shahin@icloud.com>
2026-04-05 01:01:45 +0300

    init commit

---
+++ src/test/java/ru/netology/cloudservicediploma/CloudserviceDiplomaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.cloudservicediploma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudserviceDiplomaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
